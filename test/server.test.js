4984a8ce41e33628d01efc0f5791978f91ef9b5e
emrecancc <emrecancerli55@gmail.com>
2026-07-03 07:39:06 +0300

    fix: add afterAll to close server

--- test/server.test.js
+++ test/server.test.js
@@ -1,12 +1,12 @@
-const server = require('../src/server');
+const request = require('supertest');
+const app = require('../src/app');
+const server = app.listen(3059);
 
-beforeAll(() => {
-  server.listen(3079);
+describe('Server', () => {
+  it('responds to GET /', async () => {
+    const res = await request(server).get('/');
+    expect(res.status).toBe(200);
+  });
 });
 
-afterAll(() => {
-  server.close();
-});
-
-// Existing tests
-// (The original test code remains unchanged below)
+afterAll(() => server.close());
